a33dee4477eba150ebae85534fe324a418f38047
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-11 21:47:11 +0100

    added click function

---
+++ app.js
@@ -0,0 +1,8 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator_keys')
+
+keys.addEventListener('click', e => {
+ if (e.target.matches('button')) {
+   // Do something
+ }
+})
