b1f3754d020714448847db029eedbe6c71aa2f71
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-13 21:25:47 +0100

    adding operator function

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator_keys')
 
+
 keys.addEventListener('click', e => {
- if (e.target.matches('button')) {
- }
+  if (e.target.matches('button')) {
+    // ...
+    
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-depressed')
+      // Add custom attribute
+      calculator.dataset.previousKeyType = 'operator'
+    }
+  }
 })
 
 
