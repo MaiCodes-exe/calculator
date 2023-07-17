75756aee0dd5fe101f5079cb8f7633b8458622d9
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-17 21:43:43 +0100

    removig unused functions

--- app.js
+++ app.js
@@ -27,23 +27,3 @@ if (!action) {
     console.log('number key!')
   }
 
-  if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    console.log('operator key!')
-  }
-
-  if (action === 'decimal') {
-  console.log('decimal key!')
-}
-
-if (action === 'clear') {
-  console.log('clear key!')
-}
-
-if (action === 'calculate') {
-  console.log('equal key!')
-}
